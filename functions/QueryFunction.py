3a3cd509f3ed50dede9aae22d191e4405a7da2d7
William Speirs <wspeirs@datto.com>
2016-05-06 17:01:40 -0400

    Adding query functions

---
+++ functions/QueryFunction.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+
+class QueryFunction:
+
+    def process(self, start_time:datetime, end_time:datetime):
+        """
+        Processes the function and computes the result
+        :param start_time: the starting time to look at for processing
+        :param end_time: the ending time to look at for processing
+        :return: the result of processing this function
+        """
+        pass

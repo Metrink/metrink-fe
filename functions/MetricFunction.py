68213d2d64d2974f720eecb1d31e48b7d78e80dd
William Speirs <wspeirs@datto.com>
2016-05-17 17:41:35 -0400

    Added notes for pulling data from Zabbix

--- functions/MetricFunction.py
+++ functions/MetricFunction.py
@@ -17,6 +17,8 @@ class MetricFunction(QueryFunction):
         index = []
         data = []
 
+
+
         name = "%s:%s:%s" % (self.host, self.group, self.name)
 
         while cur_time < end_time:
@@ -26,3 +28,36 @@ class MetricFunction(QueryFunction):
             cur_time += timedelta(minutes=10)
 
         return DataFrame(data, index=index)
+
+
+#
+# Zabbix Tables
+#
+#   history - stores the actual metric information
+#       itemid - links to the item the metric is for
+#       clock - the timestamp: from_unixtime(clock)
+#       value - the value of the metric
+#
+#   items - used to store "metric" names
+#       itemid - primary key
+#       name - name of the metric
+#       hostid - the id of the host it relates to
+#       description - a description of the item
+#
+#   hosts - stores the hosts on the system
+#       hostid - the unique ID of the hosts
+#       host - the name of the host
+#
+#   groups - the groups associated with hosts
+#       groupid - primary key
+#       name - the name of the group
+#
+#   hosts_groups - the mapping of hosts into groups
+#       hostgroupid - primary key
+#       hostid - the ID of the host
+#       groupid - the ID of the group
+#
+# select hostid from hosts where host = 'host'
+# select itemid from items where hostid = hostid
+# select clock, value from history where itemid = itemid
+#

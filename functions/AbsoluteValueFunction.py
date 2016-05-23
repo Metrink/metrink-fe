874d1adb8d0ab4e038a8c27ba36a4a19582928c6
William Speirs <wspeirs@datto.com>
2016-05-23 17:23:44 -0400

    Fixed abs function

--- functions/AbsoluteValueFunction.py
+++ functions/AbsoluteValueFunction.py
@@ -5,12 +5,25 @@ from functions.QueryFunction import QueryFunction
 
 class AbsoluteValueFunction(QueryFunction):
     def process(self, start_time:datetime, end_time:datetime, input:DataFrame):
-        if self._args is not None and len(self._args) > 2:
+        if (self._args is not None and len(self._args) > 2) or \
+           (len(self._args) != 0 and not isinstance(self._args[0], QueryFunction)):
             raise ValueError('Invalid argument to absolute value function')
 
-        if len(self._args) == 0:
-            # just return the abs
-            return input.abs()
-        else:
-            # process the input
-            return self._args[0].process(start_time, end_time, input).abs() if isinstance(self._args[0], QueryFunction) else abs(self._args[0])
+        # get the data
+        data = input if len(self._args) == 0 else self._args[0].process(start_time, end_time, input)
+
+        ret = None
+
+        # go through each column, get the average, and apply it to the rows
+        for col in data.columns:
+            abs = data[col].abs()  # get the absolute value for each value in the column
+            abs.name = 'abs ' + col  # update the name
+
+            if ret is None:
+                ret = DataFrame(abs)
+            else:
+                ret = ret.combine_first(DataFrame(abs))  # add it to our return value
+
+        print(ret.head())
+
+        return ret

db9f154fd70532146f1a204fb8af2acc9fbc69ed
William Speirs <wspeirs@datto.com>
2016-05-25 14:36:57 -0400

    Fixed rounding issue with resample

--- functions/MathFunction.py
+++ functions/MathFunction.py
@@ -7,24 +7,16 @@ class MathFunction(QueryFunction):
     def __init__(self, name, args):
         super().__init__(name, args)
 
-        if len(args) < 2:
-            raise ValueError("Must have at least 2 args for math functions: " + str(args))
-
     def process(self, start_time: datetime, end_time: datetime, input:DataFrame):
         if str(self._name) not in '+-*/':
             raise ValueError("Unknown math function: " + str(self._name))
 
-        if len(self._args) == 1:
-            left = input
-            i = 0
-        else:
-            left = self._args[0].process(start_time, end_time, input) if isinstance(self._args[0], QueryFunction) else self._args[0]
-            i = 1
-
         ret = DataFrame()
 
-        while i < len(self._args):
-            right = self._args[i].process(start_time, end_time, input) if isinstance(self._args[i], QueryFunction) else self._args[i]
+        # two args means we're doing A + B
+        if len(self._args) == 2:
+            left = self._args[0].process(start_time, end_time, input) if isinstance(self._args[0], QueryFunction) else self._args[0]
+            right = self._args[1].process(start_time, end_time, input) if isinstance(self._args[1], QueryFunction) else self._args[1]
 
             for l_col in left.columns:
                 for r_col in right.columns:
@@ -36,6 +28,8 @@ class MathFunction(QueryFunction):
                         t = left[l_col] * right[r_col]
                     elif self._name == '/':
                         t = left[l_col] / right[r_col]
+                    else:
+                        raise ValueError("Unknown operator: " + str(self._name))
 
                     t = DataFrame(t)
                     t.columns = [l_col + self._name + r_col]
@@ -44,7 +38,10 @@ class MathFunction(QueryFunction):
                     print(right.head())
                     print(t.head())
                     ret = ret.combine_first(t)
-            i += 1
+
+        else:  # everything is in the input DataFrame
+            ret = DataFrame(input.sum(axis=0))
+            ret.columns = [' + '.join(input.columns)]
 
         return ret
 
